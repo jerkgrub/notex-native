a66cc1bf67be0094a3ff7dbdad043441e7c08bda
JC <nucasa.jerick@gmail.com>
2024-08-22 13:28:02 +0800

    choips

--- app/(stack)/notes/[id]/view.tsx
+++ app/(stack)/notes/[id]/view.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useRouter, useLocalSearchParams, Link } from "expo-router";
-import { Plus, Square } from "@tamagui/lucide-icons";
 import { Card, ScrollView, Text } from "tamagui";
 import { Button, H3, XStack, YStack } from "tamagui";
 import AxiosInstance from "@/components/backend/AxiosInstance";
@@ -13,9 +12,9 @@ export default function View() {
   useEffect(() => {
     const fetchNote = async () => {
       try {
-        console.log(`Fetching note with id: ${id}`); // Add logging here
+        console.log(`Fetching note with id: ${id}`);
         const response = await AxiosInstance.get(`/note/${id}`);
-        console.log('Fetched note data:', response.data); // Add logging here
+        console.log('Fetched note data:', response.data);
         setNote(response.data.Note);
       } catch (error) {
         console.error("Error fetching note:", error);
@@ -24,7 +23,6 @@ export default function View() {
     if (id) fetchNote();
   }, [id]);
 
-  
   if (!note) {
     return (
       <ScrollView p={"$5"}>
@@ -36,14 +34,13 @@ export default function View() {
   return (
     <ScrollView p={"$5"}>
       <YStack mb={"$5"}>
-        {/* top container for label, search and new note button */}
         <XStack gap={"$5"} alignItems="center" justifyContent="space-between">
           <H3 style={{ whiteSpace: "nowrap" }}>
             You are now viewing {note.note_title}
           </H3>
 
           <XStack gap={"$2"}>
-            <Button bc="#CCCCCC" onPress={() => router.{'/'}()}>Back</Button>
+            <Button bc="#CCCCCC" onPress={() => router.back()}>Back</Button>
             <Link href={`/notes/${note._id}/edit`} key={note._id}>
               <Button bc="#CCCCCC">Edit Note</Button>
             </Link>
@@ -52,7 +49,7 @@ export default function View() {
       </YStack>
 
       <YStack>
-        <Card bc="lemonchiffon" width={"100%"} flex={1} height={"100vh"}>
+        <Card bc="lemonchiffon" width={"100%"} flex={1} minHeight={"200px"}>
           <Text>{note.note_content}</Text>
         </Card>
       </YStack>
